321e5718b5b0099ff06bbfe01dea72d2e1a744f9
fin444 <fin444@users.noreply.github.com>
2021-05-03 16:10:18 -0700

    More methods for TemplateBlock
    Unfortunately, this one does break serialization

--- SQCore/src/com/spacebeaverstudios/sqcore/objects/template/TemplateBlock.java
+++ SQCore/src/com/spacebeaverstudios/sqcore/objects/template/TemplateBlock.java
@@ -3,29 +3,38 @@ package com.spacebeaverstudios.sqcore.objects.template;
 import com.spacebeaverstudios.sqcore.SQCore;
 import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.block.data.BlockData;
 import org.bukkit.util.Vector;
 
 import java.io.Serializable;
 
 public class TemplateBlock implements Serializable {
-    private final Material blockType;
+    private final Material material;
     private final String blockDataString; // have to store as a String because BlockData isn't serializable
     private final int vecX, vecY, vecZ; // have to store as Integers because Vectors aren't serializable
 
     public TemplateBlock(Location location, Location origin) {
-        this.blockType = location.getBlock().getType();
+        this.material = location.getBlock().getType();
         this.blockDataString = location.getBlock().getBlockData().getAsString();
         this.vecX = location.getBlockX() - origin.getBlockX();
         this.vecY = location.getBlockY() - origin.getBlockY();
         this.vecZ = location.getBlockZ() - origin.getBlockZ();
     }
 
+    @SuppressWarnings("unused")
+    public Material getMaterial() {
+        return material;
+    }
+    @SuppressWarnings("unused")
+    public BlockData getBlockData() {
+        return SQCore.getInstance().getServer().createBlockData(blockDataString);
+    }
     public Vector getVector() {
         return new Vector(vecX, vecY, vecZ);
     }
 
     public void paste(Location loc) {
-        loc.getBlock().setType(blockType);
+        loc.getBlock().setType(material);
         loc.getBlock().setBlockData(SQCore.getInstance().getServer().createBlockData(blockDataString));
     }
 }
