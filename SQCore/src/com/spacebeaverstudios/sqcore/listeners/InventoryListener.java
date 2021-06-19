488fc99082dc36d69b8b54f5a000c9a13aabb2dc
fin444 <fin444@users.noreply.github.com>
2021-06-19 09:57:41 -0700

    Prevented modifying dynamic maps, and limited their refresh rate to once every 10 ticks

--- SQCore/src/com/spacebeaverstudios/sqcore/listeners/InventoryListener.java
+++ SQCore/src/com/spacebeaverstudios/sqcore/listeners/InventoryListener.java
@@ -3,12 +3,17 @@ package com.spacebeaverstudios.sqcore.listeners;
 import com.spacebeaverstudios.sqcore.gui.GUI;
 import com.spacebeaverstudios.sqcore.utils.GUIUtils;
 import com.spacebeaverstudios.sqcore.gui.GUIItem;
+import com.spacebeaverstudios.sqcore.utils.MapUtils;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryCloseEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.inventory.CartographyInventory;
+import org.bukkit.inventory.meta.MapMeta;
 
 public class InventoryListener implements Listener {
     @SuppressWarnings("unused")
@@ -20,6 +25,12 @@ public class InventoryListener implements Listener {
                     && GUI.getGuis().get((Player) event.getWhoClicked()).getInventory().equals(event.getClickedInventory())) {
                 GUIItem item = GUIUtils.getGUIItem(event.getCurrentItem());
                 item.runFunction((Player) event.getWhoClicked());
+            }
+        } else if (event.getClickedInventory() instanceof CartographyInventory && event.getRawSlot() == 0) {
+            if (event.getCursor().getType() == Material.FILLED_MAP
+                    && MapUtils.idsByName.containsValue(((MapMeta) event.getCursor().getItemMeta()).getMapId())) {
+                event.getWhoClicked().sendMessage(ChatColor.RED + "You aren't allowed to modify that map!");
+                event.setCancelled(true);
             }
         }
     }
--- SQCore/src/com/spacebeaverstudios/sqcore/objects/SQMapRenderer.java
+++ SQCore/src/com/spacebeaverstudios/sqcore/objects/SQMapRenderer.java
@@ -1,15 +1,29 @@
 package com.spacebeaverstudios.sqcore.objects;
 
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.map.MapCanvas;
 import org.bukkit.map.MapRenderer;
+import org.bukkit.map.MapView;
 
 public abstract class SQMapRenderer extends MapRenderer {
     public final boolean canItemFrame;
+    private int lastTickRendered = -10;
 
     public SQMapRenderer(boolean canItemFrame) {
         super(true);
         this.canItemFrame = canItemFrame;
     }
+
+    @Override
+    public void render(MapView mapView, MapCanvas mapCanvas, Player player) {
+        if (Bukkit.getCurrentTick() - 10 >= lastTickRendered) {
+            lastTickRendered = Bukkit.getCurrentTick();
+            customRender(mapView, mapCanvas, player);
+        }
+    }
+
+    public abstract void customRender(MapView mapView, MapCanvas mapCanvas, Player player);
 
     @SuppressWarnings("unused")
     protected void fillCanvas(MapCanvas mapCanvas, byte color) {
